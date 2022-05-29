4ae8fd8c719d644eb6ce448b9916f258d546cbeb
urrburu <bbs4614@naver.com>
2022-05-29 15:51:18 +0900

    spot view

--- src/main/java/com/travelwithme/withtravel/Spot/Plan.java
+++ src/main/java/com/travelwithme/withtravel/Spot/Plan.java
@@ -6,4 +6,6 @@ public class Plan{
     //Todo Plan 코드가 앞으로 spot을 대체해야함, spot-> 장소를 저장할 수는 있어야 하지만 Plan은 Travel에 종속된 저장형식 이어야함.
     //그런 Plan에서 Spot을 끌어와서 저장하는 걸로
 
+    private String longDescription;
+
 }
---
+++ src/main/java/com/travelwithme/withtravel/Spot/SpotController.java
@@ -0,0 +1,24 @@
+package com.travelwithme.withtravel.Spot;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@RequiredArgsConstructor
+public class SpotController {
+
+    private final SpotRepository spotRepository;
+
+    @GetMapping("/spots/{spotName}")
+    public String spotView(@PathVariable String spotName, Model model){
+        Spot spot = spotRepository.findBySpotName(spotName);
+        if(spot==null){
+
+        }
+        model.addAttribute(spot);
+        return "Spot/spotView";
+    }
+}
