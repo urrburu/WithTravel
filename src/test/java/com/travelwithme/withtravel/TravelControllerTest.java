9dac451f4bf11bbe9bf4e45ab1bb120e4d0a4166
urrburu <bbs4614@naver.com>
2022-05-31 17:22:45 +0900

    Travel making test fail case and travelFactory

--- src/test/java/com/travelwithme/withtravel/TravelControllerTest.java
+++ src/test/java/com/travelwithme/withtravel/TravelControllerTest.java
@@ -6,6 +6,7 @@ import com.travelwithme.withtravel.Repository.AccountRepository;
 import com.travelwithme.withtravel.Repository.TravelRepository;
 import com.travelwithme.withtravel.Travel.Travel;
 import org.apache.tomcat.jni.Local;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,25 +26,46 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class TravelControllerTest {
 
-    @Autowired    AccountRepository accountRepository;
+
     @Autowired    TravelRepository travelRepository;
     @Autowired    MockMvc mockMvc;
+
+
+    @AfterEach
+    void afterEach(){
+        travelRepository.deleteAll();
+    }
 
     @WithAccount(value = "chanhwi")
     @Test
     @DisplayName("여행 만들기 - 입력값 정상")
     public void makeTravelTestNormal() throws Exception{
-        Account account = accountRepository.findByNickname("chanhwi");
         mockMvc.perform(post("/travel/make")
                         .param("travelName", "forBusan")
                         .param("shortDescription", "short description of a travel")
-                        .param("fullDescription", "full description of a travel")
+                        .param("fullDescription", "full description of a travel full description of a travel full description of a travelfull description of a travelfull description of a travelfull description of a travel full description of a travelfull description of a travel")
                         //.param("startTime", "2022-06-07T17:03:27")
                         //.param("endTime","2022-06-25T17:03")
                         .with(csrf()))
                 .andExpect(status().is3xxRedirection());
         Travel travel = travelRepository.findByTravelName("forBusan");
         assertNotNull(travel);
+    }
+
+    @WithAccount(value = "chanchan")
+    @Test
+    @DisplayName("여행 만들기 - 입력값 너무 긴 짧은 설명")
+    public void makeTravelTestTooLongShortDescription() throws Exception{
+        mockMvc.perform(post("/travel/make")
+                        .param("travelName", "forBusan")
+                        .param("shortDescription", "short description of a travel short description of a travel short description of a travel short description of a travel short description of a travel")
+                        .param("fullDescription", "full description of a travel")
+                        //.param("startTime", "2022-06-07T17:03:27")
+                        //.param("endTime","2022-06-25T17:03")
+                        .with(csrf()))
+                .andExpect(status().is4xxClientError());
+        Travel travel = travelRepository.findByTravelName("forBusan");
+        assertNull(travel);
     }
 
 }
---
+++ src/test/java/com/travelwithme/withtravel/TravelFactory.java
@@ -0,0 +1,9 @@
+package com.travelwithme.withtravel;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TravelFactory {
+}
