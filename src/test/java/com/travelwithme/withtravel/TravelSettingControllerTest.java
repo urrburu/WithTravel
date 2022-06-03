dec954f1bfc1545d1ec5d908c9ef43f7043943dc
urrburu <bbs4614@naver.com>
2022-06-03 18:33:29 +0900

    TravelSetting Test Draft

--- src/test/java/com/travelwithme/withtravel/TravelSettingControllerTest.java
+++ src/test/java/com/travelwithme/withtravel/TravelSettingControllerTest.java
@@ -1,17 +1,28 @@
 package com.travelwithme.withtravel;
 
+import com.travelwithme.WithAccount;
+import com.travelwithme.withtravel.Account.Account;
 import com.travelwithme.withtravel.Account.AccountService;
 import com.travelwithme.withtravel.Repository.AccountRepository;
 import com.travelwithme.withtravel.Repository.TravelRepository;
 import com.travelwithme.withtravel.Settings.SettingService;
+import com.travelwithme.withtravel.Travel.Form.TravelForm;
 import com.travelwithme.withtravel.Travel.Travel;
 import com.travelwithme.withtravel.Travel.TravelService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,11 +33,21 @@ public class TravelSettingControllerTest {
     @Autowired    TravelRepository travelRepository;
     @Autowired    SettingService settingService;
     @Autowired    MockMvc mockMvc;
+    @Autowired    TravelService travelService;
 
     @BeforeEach
-    public void beforeEach(){
-        //Travel.builder().
-
+    public void beforeEach()throws Exception{
+        Account account = accountRepository.findByNickname("chanhwi");
+        if(account==null){
+            throw new Exception("wrong make");
+        }
+        TravelForm travelForm = new TravelForm();
+        travelForm.setTravelName("forBusan");
+        travelForm.setStartTime(LocalDateTime.now().plusDays(3));
+        travelForm.setEndTime(LocalDateTime.now().plusDays(7));
+        travelForm.setShortDescription("go go go go go go");
+        travelForm.setFullDescription("123456789123456789123456789123456789");
+        travelService.newTravelMake(travelForm,account);
     }
     @AfterEach
     void afterEach() {
@@ -43,5 +64,13 @@ public class TravelSettingControllerTest {
         * 이유는 여행에서 계정정보를 들고 있으니까
         *
         */
+    }
+
+    @WithAccount(value = "chanhwi")
+    @Test
+    @DisplayName("여행 본문 변경 예시")
+    private void DescriptionChange(){
+
+
     }
 }
