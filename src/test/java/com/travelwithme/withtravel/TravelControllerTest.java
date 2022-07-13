feaf97e4b621287217109cd41e21ce195a28bd30
urrburu <bbs4614@naver.com>
2022-07-13 17:20:06 +0900

    Travel test start

--- src/test/java/com/travelwithme/withtravel/TravelControllerTest.java
+++ src/test/java/com/travelwithme/withtravel/TravelControllerTest.java
@@ -1,7 +1,9 @@
 package com.travelwithme.withtravel;
 
 import com.travelwithme.WithAccount;
+import com.travelwithme.withtravel.Account.AccountRepository;
 import com.travelwithme.withtravel.Plan.PlanRepository;
+import com.travelwithme.withtravel.Travel.Travel;
 import com.travelwithme.withtravel.Travel.TravelController;
 import com.travelwithme.withtravel.Travel.TravelRepository;
 import com.travelwithme.withtravel.Travel.TravelService;
@@ -13,9 +15,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -25,6 +32,7 @@ public class TravelControllerTest {
     @Autowired    TravelController travelController;
     @Autowired    TravelRepository travelRepository;
     @Autowired    PlanRepository planRepository;
+    @Autowired AccountRepository accountRepository;
 
 
     @BeforeEach
@@ -35,21 +43,32 @@ public class TravelControllerTest {
 
     @AfterEach
     public void AfterTest(){
-
-
+        travelRepository.deleteAll();
+        accountRepository.deleteAll();
     }
     @WithAccount(value = "chanhwi")
     @Test
     @DisplayName("여행화면 만들기 화면 Get 테스트")
     public void GetMakeTravel() throws Exception{
-
-        mockMvc.perform(get("travel/newTravel"))
+        mockMvc.perform(get("/travel/newTravel"))
                 .andExpect(status().is2xxSuccessful());
     }
     @WithAccount(value = "chanhwi")
     @Test
     @DisplayName("여행화면 만들기 화면 Post 테스트")
     public void PostMakeTravel() throws Exception{
+        mockMvc.perform(post("/travel/newTravel")
+                .param("travelName", "부산여행")
+                .param("Path","rightNowPusan")
+                .param("shortDescription", "짧게 설명하기")
+                .param("fullDescription", "길게길게길게설명하기 길게길게길게설명하기 길게길게길게설명하기 길게길게길게설명하기 길게길게길게설명하기 ")
+                .param("startTime", LocalDateTime.now().plusWeeks(1).toString())
+                .param("endTime",LocalDateTime.now().plusWeeks(2).toString())
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/travel/rightNowPusan"));
+        Travel travel = travelRepository.findByPath("rightNowPusan");
+        assertFalse(travel == null);
 
     }
     @WithAccount(value = "chanhwi")
